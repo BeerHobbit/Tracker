f9e076a26e6db985fed2567e452a18783e9dced7
BeerHobbit <kovalenko537@gmail.com>
2025-11-27 19:20:04 +0300

    feat(ui): add BaseTableViewCell

---
+++ Tracker/UI/BaseTableViewCell.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    // MARK: - Views
+    
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .ypBackground
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private lazy var separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .ypGray
+        return view
+    }()
+    
+    // MARK: - Initializer
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup UI
+    
+    private func setupUI() {
+        selectionStyle = .none
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupSubviews() {
+        contentView.addSubview(containerView)
+        containerView.addSubviews([
+            hStackView,
+            separatorView
+        ])
+    }
+    
+    // MARK: - Setup Constraints
+    
+    private func setupConstraints() {
+        [
+            hStackView,
+            containerView,
+            separatorView
+        ].disableAutoresizingMasks()
+        
+        NSLayoutConstraint.activate([
+            hStackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            hStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            hStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            
+            containerView.heightAnchor.constraint(equalToConstant: 75),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            separatorView.heightAnchor.constraint(equalToConstant: 0.5),
+            separatorView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            separatorView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            separatorView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+    }
+    
+    private func configAppearance(isFirst: Bool, isLast: Bool) {
+        if isFirst {
+            containerView.layer.cornerRadius = 16
+            containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            separatorView.isHidden = false
+        }
+        if isLast {
+            containerView.layer.cornerRadius = 16
+            containerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            separatorView.isHidden = true
+        }
+    }
+    
+}
