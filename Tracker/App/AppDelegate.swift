2fbba3098174923608f34c6d41e7df117cc94afd
BeerHobbit <kovalenko537@gmail.com>
2025-12-02 11:19:58 +0300

    feat: add CoreDataStack

--- Tracker/App/AppDelegate.swift
+++ Tracker/App/AppDelegate.swift
@@ -3,8 +3,21 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    let coreDataStack = CoreDataStack()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ScheduleValueTransformer.register()
         return true
+    }
+    
+    // MARK: - UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        configuration.storyboard = nil
+        configuration.sceneClass = UIWindowScene.self
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
 
 }
