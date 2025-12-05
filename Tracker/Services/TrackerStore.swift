c9926b5dbb670d5465316b14fa262e197962e310
BeerHobbit <kovalenko537@gmail.com>
2025-12-05 18:43:15 +0300

    feat: add store classes

---
+++ Tracker/Services/TrackerStore.swift
@@ -0,0 +1,24 @@
+import UIKit
+import CoreData
+
+final class TrackerStore {
+    
+    // MARK: - Private Properties
+    
+    private let context: NSManagedObjectContext
+    
+    // MARK: - Initializer
+    
+    convenience init() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("AppDelegate is unavailable")
+        }
+        let context = appDelegate.coreDataStack.viewContext
+        self.init(context: context)
+    }
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+}
